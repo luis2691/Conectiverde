0da488bd72d1b11dbd2241d51af34e870f218230
luis2691 <luisleonbarrera@gmail.com>
2017-06-10 00:42:36 -0400

    arreglos
    deberia funcionar...

--- registrarBD.php
+++ registrarBD.php
@@ -9,10 +9,17 @@ $resultado=mysql_query( $sql ,$conexion);
 
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" type= "text/css" href="css/estiloIndex.css" />
+		
+	<title>Registrate en ConectiVerde</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+</head>
 
-	</head>
+	
 <body>
 <h1 class="titulo"> Usuario </h1>
 
